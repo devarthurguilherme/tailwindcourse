21d0fa71715a0b22eef2c09115d327fe001d538c
unknown <arthurguilherme2013@gmail.com>
2023-08-17 12:16:10 +0100

    Fonts - Typography

---
+++ Section-9/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins':'Poppins',
+        'ruwudu': 'Ruwudu',
+      },
+      fontSize: {
+        '12xl': '8rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
